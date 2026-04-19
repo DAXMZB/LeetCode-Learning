5e4887d67040883349f6e3dfd47a8bfa7f3af447
Eric Lee <tunglee1228@gmail.com>
2026-04-19 22:51:13 +0800

    weekly review

--- 198 House Robber.py
+++ 198 House Robber.py
@@ -1,29 +1,33 @@
+# we have an input array nums as Example1 
+# [1, 2, 3, 1, 9, 7] we need return the maximum amount of money  and without adjacent house
+# Fist, it need iterate though each house —> amount without adjacent
+# Use 2 viariable to stere the states —> prev2, prev1 (Initialize) 0, 0 represents the houses haven't been visited yet
+# 0, 0 -> 0, 1  prev1, (prev2 + current)      —— switch position and amount the current max
+# 2, 4 —> 4, 2 + 1 prev1, max(prev1, prev2 + current)
+# we need use max to compare pre1 and prev2 + current Because if the situation current amount is (2, 4)
+#   and the next would be 4, 2+1 and for now, the most important is 4 plus next like 9 or next plus 7 is
+#       maximum, no matter 3 plus any, so we need max to compare each prev1, prev2 whic is max and acording
+#           the max to find the maximunm , Greedy property is not enough here
 class Solution:
-    # [1, 9, 3, 1, 2]
-    # 1 —> 3
-    # 2 —> 1
-    # prev2, prev1 —> prev1, prev2 + current
-    # 0,    0           0, 1 max(prev1, prev2 + current)
-    # 0,    1           1, 9
-    # 1,    9           9, 9 
-    # 9,    9           9, 10 
-    # 9,    10         10, 11
-    # return prev1
     def rob(self, nums: List[int]) -> int:
+        # base case with boundaries
         if not nums:
             return 0
         if len(nums) == 1:
             return nums[1]
-            
+        
         prev2, prev1 = 0, 0
 
-        for n in nums:      #prev2, prev1
-            prev2, prev1 = prev1, max(prev1, prev2 + n)
+        for n in nums:
+            prev2, prev1 = prev1, max(prev1, prev2 + prev1)
         return prev1
 
-    # Time Complexity is O(n) 
-    #   Since we performs a linear scan through the nums array, 
-    #       the time complexity is proportional to the number of house
-    # Space Complexiy is O(1) 
-    #   We only maintain 2 variable (prev1, prev2) regardless of the input size, 
-    #       thus achieving constant
+        # Time Complexity is O(N)
+        #   Because we performs a linear scan through nums array
+        #       the Time Complexity is proportional to the number of house
+
+        # Space Complexity is O(1)
+        #   Because we maintain 2 viriables to store the states,
+        #       regardless of the input size
+
+        
--- 790 Domino and Tromino Tiling.py
+++ 790 Domino and Tromino Tiling.py
@@ -1,55 +1,68 @@
-# we need use 2 tile of Domino tile and Tromino tile to combination the all possible ways
+# we have 2 shape like Domino tile and Tromino tile
+# [x][x] [x][x]
+#        [x]
+
+# our task is find the Total combination through iput n
+# declare the basic case 2 by 1 and 2 by 2
+# F(n - 2) basic on n = 3
+# [ ]
+# [ ]
+
+# F(n - 1)
+# [ ][ ] with vartical and horizontal 
+# [ ][ ]
+
+# F(n) n = 3, 5 combinations
+# we need to know what F(n) are
+# F(n) = F(n - 1) + F(n - 2) + TopMissing(n - 1) + ButtomMissing(n - 1)
+# we don't multipy F(n - 2) by 2 because we want to avoid double account, 
+#   Because is already coverd by the F(n - 1) 
+#       and F(n - 2) only contribution that we haven't accounted with 2 horizontal
+
+# regardless the gree position 
+# F(n - 1) set a solution with 2 combinations
+# [ ][ ][x]
+# [ ][ ][x]
+
+# find the shape just with 1 Domino title with a solution with 1 combination
+# F(n - 2)
+# [ ][x][x]
+# [ ][x][x]
+
+# All the combination with basic F(n -1 ) and F(n - 2)
+# TopMissing(n - 1)  set solution like L shpae, we found that the solution is plus the F(n - 1) shape and with Top Missing
+# and Also need to know whoat TopMissing are like we found F(n) and basic on Domino and Tromino
+#               TopMissing(n) = ButtomMissing(n - 1) + F(n - 2)
+# [ ][x][x] ——> [ ][ ]      [ ][x]
+# [ ][ ][x]     [ ][x][x]   [ ][x][x]
+
+# Given an other L shpae we set Bottom sulution
+# BottomMissing(n - 1)
+#               ButtomMissing(n) = TopMissing(n - 1) + F(n - 2)
+# [ ][ ][x] ——> [ ][x][x]   [ ][x][x]
+# [ ][x][x]     [ ][ ]      [ ][x]
+
+# This my logic for the state transitions, 
+# i'm want make sure that does it reach an agreement ?
+#   or does any consider i'm was missing 
 class Solution:
-    # base case
-    # n = 1 
-    # [ ]
-    # [ ]
-
-    # n = 2
-    # [ ][ ] [ ][ ]
-    # [ ][ ] [ ][ ]
-    
-    # n = 3
-    # F (n - 1)
-    # [ ][ ][x] [ ][ ][x] case
-    # [ ][ ][x] [ ][ ][x]
-
-    # F (n - 2)
-    # [ ][x][x]           case
-    # [ ][x][x]
-
-    # TopMissing (n - 1)                TopMissing (n) = BottomMissing (n - 1) + F (n - 2)
-    # [ ][x][x]           case  —————>  [ ][ ]          [ ][x]
-    # [ ][ ][x]                         [ ][x][x]       [ ][x][x]
-
-    # BottomMissing (n - 1)             BottomMissing(n) = TopMissing (n -1) + F (n - 2)
-    # [ ][ ][x]           case  —————>  [ ][x][x]       [ ][x][x]
-    # [ ][x][x]                         [ ][ ]          [ ][x]
-
-    # F (n) = F (n - 1) + F (n - 2) + TopMissing (n - 1) + BottomMissing (n - 1)
     def numTilings(self, n: int) -> int:
+        # base case
         MOD = 10 ** 9 + 7
-        F, T, B = {0: 1, 1: 1}, {1: 0},{1: 0}
-        for i in range(2, n + 1):
-            F[i] = (F[i - 1] + F[i - 2] + T[i - 1] + B[i - 1]) % MOD
-            T[i] = (B[i - 1] + F[i - 2]) % MOD
-            B[i] = (T[i - 1] + F[i - 2]) % MOD
-        return F[n] 
-
-    # Time Complexity O(N)
-    #              Because we just iterate once through the loop 2 to N
-
-    # Space Complexity O(N), Because we use space due to dictionaries
-
-        MOD = 10 ** 9 + 7
-        FF,F, T, B = 1, 1, 0, 0
-        for i in range (2, n + 1):
-            FF, F, T, B = F, F + FF + T + B, B + FF, T + FF
+        
+        FF, F, T, B = 1, 1, 0, 0
+        for i in range(2, i + 1):
+            FF, F, T, B = F % MOD, (F + FF + T + B) % MOD, (B + FF) % MOD, (T + FF) % MOD
             
-        return F % MOD
+        return F
+        
+        # Time Complexity is O(N) 
+        #   we performs a linear scan through input n times
+        #       Time Complexity is depand it
 
-# Time Complexity O(N)
-#              Because we just iterate once through the loop 2 to N
+        # Space Complexity is O(N)
+        #   Because We store the states in dictionaries,
+        # 
 
 # Space Complexity O(1) Because we only maintain 4 constants
         
